8ad8af9d9f8e9474f118267dff14eb561941b376
Diego R. Mirhan <mirhan.diego@gmail.com>
2022-04-07 09:05:45 -0300

    Composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'wordpress-theme',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '414d7045a08f21faaf6dbaa4f58f3ecb37ae31a0',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'wordpress-theme',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '414d7045a08f21faaf6dbaa4f58f3ecb37ae31a0',
+            'dev_requirement' => false,
+        ),
+    ),
+);
